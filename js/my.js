c32ee6741f43ad1f2f11c15d7548c6480c368114
songtao1 <1107799536@qq.com>
2017-10-31 11:51:43 +0800

    a

--- js/my.js
+++ js/my.js
@@ -1,26 +1,19 @@
-$(function() {FastClick.attach(document.body);});
+$(function() {
+  FastClick.attach(document.body);
+});
 // getData();
 //请求数据
 function getData() {
-  $.get("test.cgi",
-  { name: "John", time: "2pm" },
-  function(data){
-    setData(d)
-  });
+  $.get("test.cgi", {
+      name: "John",
+      time: "2pm"
+    },
+    function(data) {
+      setData(d)
+    });
 }
 //加载数据
 function setData(d) {
   $(".my-comname").text("北京闪电报销");
   $(".my-phone").text("1231231312");
 }
-
-//弹起客服
-$(".my-kfbtn").click(function() {
-  $(".my-modal").css("display","block");
-  $(".cover").css("display","block");
-})
-//关闭客服
-$(".my-modal-close").click(function() {
-  $(".my-modal").css("display","none");
-  $(".cover").css("display","none");
-})
